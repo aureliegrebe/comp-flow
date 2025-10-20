72dc7052a4e2768d4229eab3e5e4eca10220916b
Aurélie Grebe <aurelie.grebe@usherbrooke.ca>
2025-10-20 16:29:37 -0400

    Added more inverse functions

--- src/mach_from.rs
+++ src/mach_from.rs
@@ -1,7 +1,10 @@
 //! Collection of functions for isentropic compressible flow.
 
-use crate::{mach_to_a_ac, mach_to_pm_angle};
-use eqsolver::single_variable::FDNewton;
+use crate::{
+    der_mach_to_f_mcpt0, der_mach_to_mcpt0_ap0, mach_to_a_ac, mach_to_f_mcpt, mach_to_mcpt0_ap0,
+    mach_to_pm_angle,
+};
+use eqsolver::single_variable::{FDNewton, Newton};
 use num::Float;
 
 /// Mach number for a given Prandtl-Meyer angle in radians.
@@ -61,18 +64,9 @@ pub fn mach_from_t_t0<F: Float>(t_t0: F, gamma: F) -> F {
 
 /// Mach number for a given total temperature ratio.
 ///
-/// # Examples
-///
-/// ```
-/// use comp_flow::mach_from_t0_t;
-///
-/// assert_eq!(mach_from_t0_t(1.0, 1.4), 0.0);
-/// assert_eq!(mach_from_t0_t(0.8333333333333334, 1.4), 1.0);
-/// assert_eq!(mach_from_t0_t(0.55555556_f32, 1.4), 2.0);
-/// ```
-pub fn mach_from_t0_t<F: Float>(t_t0: F, gamma: F) -> F {
+pub fn mach_from_t0_t<F: Float>(t0_t: F, gamma: F) -> F {
     let two = F::from(2.0).unwrap();
-    (two / (gamma - F::one()) * (F::one() / t_t0 - F::one())).sqrt()
+    (two / (gamma - F::one()) * (t0_t - F::one())).sqrt()
 }
 
 /// Mach number for a given total pressure ratio.
@@ -91,6 +85,11 @@ pub fn mach_from_p_p0<F: Float>(p_p0: F, gamma: F) -> F {
     (two / (gamma - F::one()) * (p_p0.powf((F::one() - gamma) / gamma) - F::one())).sqrt()
 }
 
+pub fn mach_from_p0_p<F: Float>(p0_p: F, gamma: F) -> F {
+    let two = F::from(2.0).unwrap();
+    (two / (gamma - F::one()) * (p0_p.powf((gamma - F::one()) / gamma) - F::one())).sqrt()
+}
+
 /// Mach number for a given stagnation density ratio.
 ///
 /// # Examples
@@ -105,6 +104,43 @@ pub fn mach_from_p_p0<F: Float>(p_p0: F, gamma: F) -> F {
 pub fn mach_from_rho_rho0<F: Float>(rho_rho0: F, gamma: F) -> F {
     let two = F::from(2.0).unwrap();
     (two / (gamma - F::one()) * (rho_rho0.powf(F::one() - gamma) - F::one())).sqrt()
+}
+
+pub fn mach_from_rho0_rho<F: Float>(rho0_rho: F, gamma: F) -> F {
+    let two = F::from(2.0).unwrap();
+    (two / (gamma - F::one()) * (rho0_rho.powf(gamma - F::one()) - F::one())).sqrt()
+}
+
+pub fn mach_from_v_cpt0<F: Float>(v_cpt0: F, gamma: F) -> F {
+    let half = F::from(0.5).unwrap();
+    (F::one() / (gamma - F::one()) * v_cpt0.powi(2) / (F::one() - half * v_cpt0.powi(2))).sqrt()
+}
+
+pub fn mach_from_f_mcpt0<F: Float>(f_mcpt0: F, gamma: F, supersonic: bool) -> F {
+    let f = |m| mach_to_f_mcpt(m, gamma);
+    let df = |m| der_mach_to_f_mcpt0(m, gamma);
+    let x0 = match supersonic {
+        true => F::from(1.5).unwrap(),
+        false => F::from(0.5).unwrap(),
+    };
+    Newton::new(f, df).with_itermax(100).solve(x0).unwrap()
+}
+
+pub fn mach_from_mcpt0_ap0<F: Float>(f_mcpt0: F, gamma: F, supersonic: bool) -> F {
+    let f = |m| mach_to_mcpt0_ap0(m, gamma);
+    let df = |m| der_mach_to_mcpt0_ap0(m, gamma);
+    let x0 = match supersonic {
+        true => F::from(1.5).unwrap(),
+        false => F::from(0.5).unwrap(),
+    };
+    Newton::new(f, df).with_itermax(100).solve(x0).unwrap()
+}
+
+pub fn mach_from_mcpt0_ap<F: Float>(f_mcpt0: F, gamma: F) -> F {
+    let f = |m| mach_to_mcpt0_ap(m, gamma);
+    let df = |m| der_mach_to_mcpt0_ap(m, gamma);
+    let x0 = F::from(0.5).unwrap();
+    Newton::new(f, df).with_itermax(100).solve(x0).unwrap()
 }
 
 /// Mach number for a given critical area ratio.
